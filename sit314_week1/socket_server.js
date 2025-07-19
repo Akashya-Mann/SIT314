365a81b88175b3aa37523b1c4ef60ec1b5d5b826
Akashya-Mann <s224597443@deakin.edu.au>
2025-07-20 02:26:36 +1000

    Update socket_server.js

--- sit314_week1/socket_server.js
+++ sit314_week1/socket_server.js
@@ -1,21 +1,17 @@
 //socket_server.py
 const net = require("net");
-
 const port = 5000;
-
 const server = net.createServer((socket) => {
+    
     console.log("Client connected");
-
     socket.on("data", (data) => {
         const strData = data.toString();
         console.log(`Received: ${strData}`);
-
         const command = strData.split(",");
         const operator = command[0];
         const operand1 = parseFloat(command[1]);
         const operand2 = parseFloat(command[2]);
         let result;
-
         switch (operator) {
             case "add":
                 result = operand1 + operand2;
@@ -25,10 +21,10 @@ const server = net.createServer((socket) => {
                 break;         
                 //Modify the server to support different calculations.
             case "multiply":
-                result = operand1 * operand2; // Adding a Multiplication operations q2
+                result = operand1 * operand2; //Adding a Multiplication operations q2
                 break;
             case "divide":
-                result = operand1 / operand2; // Dividing two operations q2
+                result = operand1 / operand2; //Dividing the two operations q2
                 break;
 
 
